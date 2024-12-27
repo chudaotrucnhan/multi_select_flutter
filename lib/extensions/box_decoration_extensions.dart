abefccc4d98910069cda7c3e88ccf0eae1c0216b
Nhân Chu <nhan.chu@idtek.com.vn>
2024-12-27 15:33:48 +0700

    Update box_decoration_extensions.dart

--- lib/extensions/box_decoration_extensions.dart
+++ lib/extensions/box_decoration_extensions.dart
@@ -3,18 +3,13 @@ import 'package:flutter/material.dart';
 extension BoxDecorationExtensions on BoxDecoration {
   BoxDecoration merge(BoxDecoration? other) {
     return BoxDecoration(
-      color: color ?? other?.color,
-      image: image ?? other?.image,
-      border: border ?? other?.border,
-      borderRadius: borderRadius ?? other?.borderRadius,
-      boxShadow: boxShadow ?? other?.boxShadow,
-      gradient: gradient ?? other?.gradient,
-      backgroundBlendMode: backgroundBlendMode ?? other?.backgroundBlendMode,
-      shape: shape != null
-          ? shape
-          : other?.shape != null
-              ? other!.shape
-              : BoxShape.rectangle,
-    );
+        color: other?.color ?? color,
+        image: other?.image ?? image,
+        border: other?.border ?? border,
+        borderRadius: other?.borderRadius ?? borderRadius,
+        boxShadow: other?.boxShadow ?? boxShadow,
+        gradient: other?.gradient ?? gradient,
+        backgroundBlendMode: other?.backgroundBlendMode ?? backgroundBlendMode,
+        shape: other?.shape ?? shape);
   }
 }
